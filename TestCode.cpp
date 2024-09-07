6bc6b02fb2cbc25a128e99204882a6005b4fc270
SyedAucf <sy961821@ucf.edu>
2024-09-07 16:44:53 +0000

    testingagain

--- TestCode.cpp
+++ TestCode.cpp
@@ -1,13 +1,11 @@
 //funsies code
 
-void printSum(int num1, int num2){
-
+void printSum(int num1, int num2)
+{
 printf("The sum of the following numbers is %d\n", num1 + num2);
-
 }
 
-int main(void){
-
-    printSum(2, 3);
-
+int main(void)
+{
+ printSum(2, 3);
 }
