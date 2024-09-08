5eaad5575e3fbd08f084134531f51a295c052548
SyedAucf <sy961821@ucf.edu>
2024-09-08 19:39:32 +0000

    comits

--- fracturingREDO(renameAfterFix).c
+++ fracturingREDO(renameAfterFix).c
@@ -41,16 +41,21 @@ double calculateDistance()
 
     printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
     printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+
+    double distance = distanceFormula(x1, x2, y1, y2);
+    printf("The distance between the two points is %.2f\n", distance);
+
+    return 0;
 }
 
 
 int main(int argc, char **argv)
 {
     calculateDistance();
-    calculatePerimeter();
-    calculateArea();
-    calculateWidth();
-    calculateHeight();
+    //calculatePerimeter();
+    //calculateArea();
+    //calculateWidth();
+    //calculateHeight();
 
     return 0;
 }
